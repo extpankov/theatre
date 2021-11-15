497387a5c2c0cfda535338a25fa98d2c3a852b6e
extpankov <RomchanskiyR@yandex.ru>
2021-11-15 20:42:48 +0300

    feat: add drop-down menu for small size displays

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-import css from "../src/style.min.css"
+let a = false
+$('.header__menu').click(() => {
+    if ($('.header__ddm').css('display') == 'block') {
+        $('.header__ddm').hide()
+    } else {
+        $('.header__ddm').show()
+    }
+})

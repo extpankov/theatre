606b0cfb80484b216e699205fc0bbe100a490f4d
extpankov <RomchanskiyR@yandex.ru>
2021-11-21 01:33:43 +0300

    feat: webpack setup, final style fixes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-let a = false
+import css from "../src/style.min.css"
+
 $('.header__menu').click(() => {
     if ($('.header__ddm').css('display') == 'block') {
         $('.header__ddm').hide()
     } else {
         $('.header__ddm').show()
     }
+})
+
+$('.sn__a').click(() => {
+    event.preventDefault()
 })
